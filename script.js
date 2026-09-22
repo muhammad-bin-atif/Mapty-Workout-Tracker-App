09c9bc221222f6fcde99f6320fdc26fad4f61625
Muhammad Bin Atif <huzaifa7893131@gmail.com>
2026-09-22 09:12:46 +0500

    Add Workout classes for Running and Cycling

--- script.js
+++ script.js
@@ -11,6 +11,48 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; // in km
+    this.duration = duration; // in min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+///////////////////////////////////////////////
+// Application Architecture
+
 class App {
   #map;
   #mapEvent;
@@ -80,7 +122,7 @@ class App {
       .bindPopup(
         L.popup({
           maxWidth: 250,
-          maxWidth: 100,
+          minWidth: 100,
           autoClose: false,
           closeOnClick: false,
           className: 'running-popup',
